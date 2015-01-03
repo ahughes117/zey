1ab7546e37718401aa6631d859c16f0137d81110
Alex Hughes <ahughes@ahughes.org>
2015-01-03 16:20:30 +0000

    Added project files for Java Client

---
+++ client/ZEYClient/src/zeyclient/Main.java
@@ -0,0 +1,17 @@
+package zeyclient;
+
+/**
+ * The Main Class of the ZEY Java Client
+ *
+ * @author ahughes
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
